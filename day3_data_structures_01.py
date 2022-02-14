cd897a8ea78270565b7ccaa902117aa1e928c7bf
fitosky1 <fitobriones@gmail.com>
2022-02-14 09:19:29 +0100

    data structures ABC day3

---
+++ day3_data_structures_01.py
@@ -0,0 +1,16 @@
+'''
+Data structures
+'''
+# Lists or also arrays in python
+weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+
+# Tests
+print(weekdays[1])
