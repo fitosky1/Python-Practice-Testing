31e27769ded22cd65a4fc60880f08f7e51a7dc00
fitosky1 <fitobriones@gmail.com>
2022-02-15 13:44:05 +0100

    more data structures day4

---
+++ day4_data_structures_11_dictionaries.py
@@ -0,0 +1,16 @@
+"""
+Data structures
+Dictionaries
+"""
+dict1 = {}
+#
+# dictionary = {"key1": value1, "key2": value2}
+dict1 = {"key1": 10, "key2": 20}
+dict1["A"] = "rrr"
+dict1["C"] = 321
+#
+print("dict1 ", dict1)
+print("dict1 ", dict1.keys())
+print("dict1 ", dict1["C"])
+
+print(type(dict1))
